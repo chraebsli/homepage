71ae18b5355aff813975c52527ca9e02095fb459
chraebsli <nicholas.joel.krebs@gmail.com>
2022-04-08 09:55:16 +0200

    update navbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Navbar from "./App";
+import Navbar from "./components/Navbar/Navbar";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
