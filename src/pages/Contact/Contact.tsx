02a900fd5f6aadfa9072b69066d20afd7f04d9b8
chraebsli <nicholas.joel.krebs@gmail.com>
2022-06-09 17:41:18 +0200

    update contact

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,26 +1,30 @@
 // import libraries
 import React from "react";
+import { Container } from "react-bootstrap";
 
 // import assets and css
 import "./Contact.css";
 
-// render the page kontakt
+// render the page contact
 const Contact = () => (
-	<main className="contact">
-		<div className="header">
-			<p className="title-text">Kontakt</p>
-		</div>
+	<main className={ "contact" }>
+		<Container>
+			<div className={ "page-title" }>
+				<p>Über mich</p>
+			</div>
 
-		<div className="content f col">
-			<section>
-				<p className="section-title">Kontaktieren Sie mich</p>
-				<p className="section-text">
-					Wenn Sie Fragen oder Anmerkungen haben, wenden Sie sich bitte an{ " " }
-					<a href="mailto:contact@chraebsli.dev">contact@chraebsli.dev</a>. Ich beantworte gerne Ihre Fragen
-					und freue mich über Feedback jeglicher Art.
-				</p>
-			</section>
-		</div>
+			<div className="page-content d-flex justify-content-center">
+				<section>
+					<p className="section-title">Kontaktieren Sie mich</p>
+					<p className="section-text">
+						Wenn Sie Fragen oder Anmerkungen haben, wenden Sie sich bitte an{ " " }
+						<a href="mailto:contact@chraebsli.dev">contact@chraebsli.dev</a>. Ich beantworte gerne Ihre
+						Fragen
+						und freue mich über Feedback jeglicher Art.
+					</p>
+				</section>
+			</div>
+		</Container>
 	</main>
 );
 
