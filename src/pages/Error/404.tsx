7fe06e91266b35cee667916715ef7a2a5040049a
chraebsli <nicholas.joel.krebs@gmail.com>
2022-09-10 22:31:09 +0200

    fix path error

--- src/pages/Error/404.tsx
+++ src/pages/Error/404.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { Container, Stack, Typography } from "@mui/material";
 import Head from "../../components/common/Head";
-import { Image, Line, PageTitle, SectionTitle } from "@components/Text";
+import { Image, Line, PageTitle, SectionTitle } from "../../components/Text";
 
 // assets
 import ImageError404 from "./assets/404.jpg";
 
-export default function NotFound404() {
+export default function NotFound404(){
 	const pageName = "Error 404";
 	return (
 		<>
-			<Head title={pageName} />
-			<main className={"page-content"}>
+			<Head title={ pageName } />
+			<main className={ "page-content" }>
 				<Container>
 					<section>
-						<PageTitle>{pageName}</PageTitle>
-						<Line bottom={2} />
+						<PageTitle>{ pageName }</PageTitle>
+						<Line bottom={ 2 } />
 					</section>
 
 					<article className="page-content">
-						<Image src={ImageError404} alt={"Illustration Datenbank"} height={200} m />
-						<Stack spacing={3}>
+						<Image src={ ImageError404 } alt={ "Illustration Datenbank" } height={ 200 } m />
+						<Stack spacing={ 3 }>
 							<section>
 								<SectionTitle>Error 404: Die Seite existiert nicht.</SectionTitle>
 								<Typography>
