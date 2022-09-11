21bc66df328eb6549533d6a4034926c6c4eb63df
chraebsli <nicholas.joel.krebs@gmail.com>
2022-09-11 11:18:40 +0200

    better progress bar

--- src/components/me/Progress.tsx
+++ src/components/me/Progress.tsx
@@ -1,23 +1,20 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, LinearProgress, Stack, Typography } from "@mui/material";
 
 export default function Progress({
-	text,
-	icon,
-	progress,
-	max = 100,
-}: {
-	text: string;
-	icon: React.ReactNode;
-	progress: number;
-	max?: number;
-}) {
+	text, icon, progress,
+}: { text: string; icon: React.ReactNode; progress: number; }) {
 	return (
-		<Box>
-			<Typography component={ "span" }>
-				{ icon } { text }
-			</Typography>
-			<progress className={ "progress" } value={ progress } max={ max } />
+		<Box sx={ { mb: ".5rem", alignItems: "center" } }>
+			<Typography component={ "span" }>{ icon } { text }</Typography>
+			<Stack direction={ "row" } spacing={ 2 } alignItems={ "center" }>
+				<Box sx={ { width: "100%", mr: 0 } }>
+					<LinearProgress variant={ "determinate" } value={ progress } sx={ { p: ".5rem" } } />
+				</Box>
+				<Box sx={ { minWidth: "2rem" } }>
+					<Typography>{ progress }% </Typography>
+				</Box>
+			</Stack>
 		</Box>
 	);
 }
