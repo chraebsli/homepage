3ccf64f526addc9064fac2a588e98bd17a83099f
chraebsli <nicholas.joel.krebs@gmail.com>
2022-09-30 13:47:48 +0200

    add locales for next-i18next

---
+++ next-i18next.config.ts
@@ -0,0 +1,23 @@
+const nextI18nextConfig = {
+	i18n: {
+		defaultLocale: "de",
+		locales: [ "en", "de" ],
+		//domains: [
+		//	{
+		//		domain: "www.chraebsli.dev",
+		//		defaultLocale: "en",
+		//	},
+		//	{
+		//		domain: "www.nicholas-krebs.ch",
+		//		defaultLocale: "de",
+		//	},
+		//	{
+		//		domain: "localhost",
+		//		defaultLocale: "de",
+		//		http: true,
+		//	},
+		//],
+	},
+};
+
+export default nextI18nextConfig;
